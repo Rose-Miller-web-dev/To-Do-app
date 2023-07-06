1a816427aeae219fce48b64af5e854eb56d214d2
rose-miller-web-dev <rosemillerupworkfreelancer@gmail.com>
2023-07-06 08:55:39 -0400

    rest_framework completed

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('rest_framework_get_tasks/', views.get_tasks, name="drf_get_tasks"),
+    path('rest_framework_get_task_detail/<str:pk>/', views.get_task_detail, name="drf_get_task_detail"),
+    path('rest_framework_create_task/', views.create_task, name="drf_create_task"),
+    path('rest_framework_update_task/<str:pk>/', views.update_task, name="drf_update_task"),
+    path('rest_framework_delete_task/<str:pk>/', views.delete_task, name="drf_delete_task")
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
@@ -18,3 +18,36 @@ def get_tasks(request):
 
     serialized_data = TaskSerializer(tasks, many=True)
     return Response(serialized_data.data)
+
+@api_view(['GET'])
+def get_task_detail(request, pk):
+    task = Task.objects.get(id=pk)
+    serialized_data = TaskSerializer(task, many=False)
+    return Response(serialized_data.data)
+
+@api_view(['POST'])
+def create_task(request):
+    serialized_data = TaskSerializer(data=request.data)
+
+    if serialized_data.is_valid():
+        serialized_data.save()
+
+    return Response(serialized_data.data)
+
+@api_view(['POST'])
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+    
+    task.delete()
+
+    return redirect('drf_get_tasks')
